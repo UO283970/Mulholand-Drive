fb8cb1549023bf8a2d4e2322af57a5071a6d670f
UO283970 <98962779+UO283970@users.noreply.github.com>
2023-11-06 15:43:15 +0100

    Habitación creada

---
+++ WhoAmI/Assets/Resources/Scripts/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+///--------------------------------
+///   Author: Victor Alvarez, Ph.D.
+///   University of Oviedo, Spain
+///--------------------------------
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 8f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 2f;
+
+    Vector3 velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump"))
+            velocity.y = Mathf.Sqrt (jumpHeight * -2f * gravity);
+            
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+        private void OnControllerColliderHit(ControllerColliderHit hit) {
+        if (hit.gameObject.CompareTag("Collectable"))
+        {
+            //inventario.AddItemToInv(hit.gameObject);
+            Debug.Log("Buuenas");
+            Destroy(this);
+        } 
+    }
+}
