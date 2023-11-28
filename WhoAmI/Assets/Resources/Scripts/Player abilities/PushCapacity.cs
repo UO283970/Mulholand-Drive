96838938f1bf4f6f43dfa9d4873061ea30e14d3e
Pelayo-Reguera <UO282348@uniovi.es>
2023-11-28 04:00:58 +0100

    Script para poder empujar objetos

---
+++ WhoAmI/Assets/Resources/Scripts/Player abilities/PushCapacity.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushCapacity : MonoBehaviour
+{
+    public float pushPower = 2.0F;
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+
+        // no rigidbody
+        if (body == null || body.isKinematic)
+            return;
+
+        // We dont want to push objects below us
+        if (hit.moveDirection.y < -0.3f)
+            return;
+
+        // Calculate push direction from move direction,
+        // we only push objects to the sides never up and down
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+        // If you know how fast your character is trying to move,
+        // then you can also multiply the push velocity by that.
+
+        // Apply the push
+        body.velocity = pushDir * pushPower / (body.mass);
+
+        //Apply the sound effect
+        AudioSource audio = hit.gameObject.GetComponent<AudioSource>();
+        if(audio != null){
+            audio.Play();
+        }
+    }
+}
